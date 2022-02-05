d830e8ef05979f668dd545d7acd8e7f062e2d5ae
Artur Veselovski <arveselovski@gmail.com>
2022-02-05 12:05:06 +0200

    removed old mongoose model

--- models/project.ts
+++ models/project.ts
@@ -1,77 +1,79 @@
-import mongoose from "mongoose";
+export {};
 
-import { IGroup, ITask } from "types";
+// import mongoose from "mongoose";
 
-const taskSchema = new mongoose.Schema<ITask>(
-  {
-    /** Task title */
-    title: {
-      type: String,
-      required: [true, "Task needs a title"],
-      trim: true,
-      maxlength: [120, "Task title cannot exceed the length of 120 characters."],
-    },
+// import { IGroup, ITask } from "types";
 
-    /** Task body, markdown content */
-    body: {
-      type: String,
-      required: [true, "Task needs a body"],
-      trim: true,
-    },
+// const taskSchema = new mongoose.Schema<ITask>(
+//   {
+//     /** Task title */
+//     title: {
+//       type: String,
+//       required: [true, "Task needs a title"],
+//       trim: true,
+//       maxlength: [120, "Task title cannot exceed the length of 120 characters."],
+//     },
 
-    _groupId: {
-      type: String,
-    },
-  },
+//     /** Task body, markdown content */
+//     body: {
+//       type: String,
+//       required: [true, "Task needs a body"],
+//       trim: true,
+//     },
 
-  { timestamps: true }
-);
+//     _groupId: {
+//       type: String,
+//     },
+//   },
 
-const taskGroupSchema = new mongoose.Schema<IGroup>(
-  {
-    /** Task group title */
-    title: {
-      type: String,
-      required: [true, "Task group needs a title"],
-      trim: true,
-      maxlength: [60, "Task group title cannot exceed the length of 60 characters."],
-    },
+//   { timestamps: true }
+// );
 
-    /** Tasks (subdocument) */
-    tasks: [taskSchema],
-  },
+// const taskGroupSchema = new mongoose.Schema<IGroup>(
+//   {
+//     /** Task group title */
+//     title: {
+//       type: String,
+//       required: [true, "Task group needs a title"],
+//       trim: true,
+//       maxlength: [60, "Task group title cannot exceed the length of 60 characters."],
+//     },
 
-  { timestamps: true }
-);
+//     /** Tasks (subdocument) */
+//     tasks: [taskSchema],
+//   },
 
-interface IProject {
-  title: string;
-  version?: number;
-  taskGroups: IGroup[];
-}
+//   { timestamps: true }
+// );
 
-const projectSchema = new mongoose.Schema<IProject>(
-  {
-    /** Project title */
-    title: {
-      type: String,
-      required: [true, "Project needs a title."],
-      trim: true,
-      minlength: [3, "Project title must be at least 3 characters long."],
-      maxlength: [60, "Project title cannot exceed the length of 60 characters."],
-    },
+// interface IProject {
+//   title: string;
+//   version?: number;
+//   taskGroups: IGroup[];
+// }
 
-    /** Project version (for future implementation) */
-    version: {
-      type: Number,
-      default: 1,
-    },
+// const projectSchema = new mongoose.Schema<IProject>(
+//   {
+//     /** Project title */
+//     title: {
+//       type: String,
+//       required: [true, "Project needs a title."],
+//       trim: true,
+//       minlength: [3, "Project title must be at least 3 characters long."],
+//       maxlength: [60, "Project title cannot exceed the length of 60 characters."],
+//     },
 
-    /** Task groups (subdocument) */
-    taskGroups: [taskGroupSchema],
-  },
+//     /** Project version (for future implementation) */
+//     version: {
+//       type: Number,
+//       default: 1,
+//     },
 
-  { timestamps: true }
-);
+//     /** Task groups (subdocument) */
+//     taskGroups: [taskGroupSchema],
+//   },
 
-export default mongoose.models.Project || mongoose.model<IProject>("Project", projectSchema);
+//   { timestamps: true }
+// );
+
+// export default mongoose.models.Project || mongoose.model<IProject>("Project", projectSchema);
